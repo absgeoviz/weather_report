622ae50196357dbcfde070893e2e693a0f31b0e0
absgeoviz <155337188+absgeoviz@users.noreply.github.com>
2024-09-30 21:09:28 +0800

    Create options.js

---
+++ options.js
@@ -0,0 +1,65 @@
+var options = {
+    "chart": {
+        "height": 350,
+        "type": "heatmap"
+    },
+    "series": [
+        {
+            "name": "Metric1",
+            "data": [
+                {"x": "Value1", "y": 10},
+                {"x": "Value2", "y": 20},
+                {"x": "Value3", "y": 30},
+                {"x": "Value4", "y": 40},
+                {"x": "Value5", "y": 50}
+            ]
+        },
+        {
+            "name": "Metric2",
+            "data": [
+                {"x": "Value1", "y": 15},
+                {"x": "Value2", "y": 25},
+                {"x": "Value3", "y": 35},
+                {"x": "Value4", "y": 45},
+                {"x": "Value5", "y": 55}
+            ]
+        },
+        {
+            "name": "Metric3",
+            "data": [
+                {"x": "Value1", "y": 20},
+                {"x": "Value2", "y": 30},
+                {"x": "Value3", "y": 40},
+                {"x": "Value4", "y": 50},
+                {"x": "Value5", "y": 60}
+            ]
+        },
+        {
+            "name": "Metric4",
+            "data": [
+                {"x": "Value1", "y": 25},
+                {"x": "Value2", "y": 35},
+                {"x": "Value3", "y": 45},
+                {"x": "Value4", "y": 55},
+                {"x": "Value5", "y": 65}
+            ]
+        },
+        {
+            "name": "Metric5",
+            "data": [
+                {"x": "Value1", "y": 30},
+                {"x": "Value2", "y": 40},
+                {"x": "Value3", "y": 50},
+                {"x": "Value4", "y": 60},
+                {"x": "Value5", "y": 70}
+            ]
+        }
+    ],
+    "dataLabels": {
+        "enabled": false
+    },
+    "colors": ["#008FFB"],
+    "title": {
+        "text": "Heatmap Chart"
+    }
+};
